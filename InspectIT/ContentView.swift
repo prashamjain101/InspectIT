ca43233468c33063489a81b53babd9454a1087a6
Prasham <suyashjain004@gmail.com>
2024-06-20 17:14:44 +0530

    Initial Commit

---
+++ InspectIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InspectIT
+//
+//  Created by Prasham Jain on 20/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InspectIT/InspectITApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspectITApp.swift
+//  InspectIT
+//
+//  Created by Prasham Jain on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InspectITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
